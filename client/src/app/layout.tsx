8d5861a5aa33e2a8463ed01dba3aa59963bb7cd7
Simran <itssimran903@gmail.com>
2025-06-25 03:29:28 +0530

    fix: add suppressHydrationWarning to RootLayout for improved rendering

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout({
       <ThemeProvider>
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          suppressHydrationWarning={true}
         >
           {children}
         </body>
